097ecd3e1cc0216c8c6acf5ec1815fb0b35cd30a
Janvi Dabhi <dabhijanvi681@gmail.com>
2025-06-01 22:30:38 +0530

    Add files via upload

---
+++ js18.js
@@ -0,0 +1,6 @@
+document.querySelectorAll('.read-more').forEach(button => {
+  button.addEventListener('click', () => {
+    alert('Redirecting to the full post...');
+    // Here, you can add functionality to redirect to the full post page
+  });
+});
